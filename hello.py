866dd2e3642212806e33d81e69c772e1ed2a9d0e
Nada Abbassi <nada@Sansfil-Eduroam-Externe-208-65.polymtl.ca>
2024-09-04 20:11:19 -0400

    Réponse à la question du TP0 complétée

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
+aire_cercle= math.pi * (rayon)**2 
+print(f"L'aire d'un cercle de rayon {rayon} est {aire_cercle}")
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Nada Abbassi")
